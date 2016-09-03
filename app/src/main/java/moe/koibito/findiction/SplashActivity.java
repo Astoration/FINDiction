a87850f775f21623c98645a4127afd3c2cfa65b3
Astoration <ybdh00@naver.com>
2016-09-03 19:43:26 +0900

    add splash activity

---
+++ app/src/main/java/moe/koibito/findiction/SplashActivity.java
@@ -0,0 +1,31 @@
+package moe.koibito.findiction;
+
+import android.app.Activity;
+import android.app.Notification;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+
+/**
+ * Created by Astoration on 2016. 9. 3..
+ */
+public class SplashActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.spash_layout);
+        Handler handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+
+        handler.sendEmptyMessageDelayed(0, 5000);
+    }
+}
